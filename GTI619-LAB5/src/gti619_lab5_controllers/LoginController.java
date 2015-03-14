a958fc20b55df57d0b48dfb6fc013786d2f97774
r.registe <r.registe@I3Reggie-PC.imago.local>
2015-03-13 20:17:59 -0400

    [RAU] LoginController just to understand how to do

---
+++ GTI619-LAB5/src/gti619_lab5_controllers/LoginController.java
@@ -0,0 +1,11 @@
+package gti619_lab5_controllers;
+
+import javax.servlet.http.HttpServlet;
+
+public class LoginController extends HttpServlet {
+
+	public LoginController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
