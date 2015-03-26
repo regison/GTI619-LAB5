e9559f23dbd1bb0208b58cab0abcd61db22ae8e1
r.registe <r.registe@I3Reggie-PC.imago.local>
2015-03-26 15:19:27 -0400

    [RAU] Update le user avec 2 attributs : isAuthenticate, isLogoutNeeded

--- GTI619-LAB5/src/communication/DataObjects/QueryFactory.java
+++ GTI619-LAB5/src/communication/DataObjects/QueryFactory.java
@@ -3,20 +3,22 @@ package communication.DataObjects;
 public class QueryFactory {
 
 	//SELECT SECTION
-	public static final String SELECT_ALL_USERS = "SELECT * FROM User";
-	public static final String SELECT_ALL_ROLE_RIGHTS = "SELECT * FROM RoleLevel";
+	public static final String SELECT_ALL_USERS = "SELECT u.*, ll.LoggedIn, ll.LogoutNeeded FROM User u INNER JOIN LoginLog ll ON u.idUser = ll.idUser";
+	public static final String SELECT_ALL_ROLELEVEL = "SELECT * FROM RoleLevel";
 	public static final String SELECT_ALL_ROLES = "SELECT * FROM RoleLevel";
-	public static final String SELECT_ALL_LOGS = "SELECT * FROM LOGS";
+	public static final String SELECT_ALL_LOGS = "SELECT * FROM Log";
+	public static final String SELECT_ALL_LOGINLOGS = "SELECT * FROM LoginLog";
 	
-	public static final String SELECT_USER = "SELECT * FROM User WHERE name='{0]' and password='{1}";
-	public static final String SELECT_USER_ROLES = "SELECT * FROM ROLE WHERE roleId = {0]";	
-	public static final String SELECT_USER_ROLE_LEVEL = "SELECT * FROM RoleLevel WHERE roleid = {0}";
+	public static final String SELECT_USER_BY_UNAME_PWD = "SELECT * FROM User WHERE name= ? and password= ?";
+	public static final String SELECT_USER_BYID = "SELECT u.* , ll.LoggedIn, ll.LogoutNeeded FROM User u INNER JOIN LoginLog ll ON u.idUser = ll.idUser WHERE u.idUser = ? ;";
+	public static final String SELECT_USER_ROLE = "SELECT * FROM ROLE WHERE idRole = ? ;";	
+	public static final String SELECT_USER_ROLELEVEL = "SELECT * FROM RoleLevel WHERE idRoleLevel = ? ;";
 	
 	
 	//UPDATE SECTION
 	
 	
-	//INSERT SECTION
-	
+	//INSERT SECTION	
 	public static final String INSERT_LOG = "INSERT INTO Log Values ({0},'{1}','{2}',{3})";
+	public static final String INSERT_LOGINLOG = "INSERT INTO LoginLog Values ({0},'{1}','{2}',{3})";
 }
